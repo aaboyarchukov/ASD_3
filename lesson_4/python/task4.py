1262ad238dc9aae82f5fa2ce8dd3f6e69cf0c5c8
aaboyarchukov <boyarchukov30@gmail.com>
2026-01-25 21:06:39 +0300

    add lesson 4 solution

---
+++ lesson_4/python/task4.py
@@ -0,0 +1,33 @@
+def ArrayChunk(M):
+    size = len(M)
+    if size < 2:
+        return 0
+    
+    i1, i2 = 0, size-1
+    basic_indx = size // 2
+    N = M[basic_indx]
+
+    while i1 < i2:
+        
+        while M[i1] < N:
+            i1+=1
+
+        while M[i2] > N:
+            i2-=1
+        
+        if i1 == i2 - 1 and M[i1] > M[i2]:
+            M[i1], M[i2] = M[i2], M[i1]
+        
+        if i1 == i2 - 1 and M[i1] < M[i2]:
+            return basic_indx
+        
+        M[i1], M[i2] = M[i2], M[i1]
+
+        if M[i1] == N:
+            basic_indx = i1
+        if M[i2] == N:
+            basic_indx = i2
+    
+    return basic_indx
+        
+        
---
+++ lesson_4/python/task4_3.py
@@ -0,0 +1,36 @@
+import unittest
+from task4 import ArrayChunk
+
+class TestBuildArrayChunk(unittest.TestCase):
+    def test_build_array_chunk(self):
+        cases = [
+            ([], [], 0),
+            ([1], [1], 0),
+            ([40, 13, 4, 1], [1, 4, 13, 40], 1),
+            ([7, 5, 6, 4, 3, 1, 2], [2, 1, 3, 4, 6, 5, 7], 3),
+            ([121, 40, 13, 4, 1], [1, 4, 13, 40, 121], 2),
+        ]
+
+        for get, want, want_indx in cases:
+            with self.subTest(get=get):
+                indx = ArrayChunk(get)
+                self.assertEqual(
+                    get, want,
+                    msg=(
+                        "FAIL: ArrayChunk неверно разделил массив на группы.\n"
+                        f"Ожидалось: {want}\n"
+                        f"Получено:  {get}"
+                    )
+                )
+
+                self.assertEqual(
+                    indx, want_indx,
+                    msg=(
+                        "FAIL: ArrayChunk неверно вернул индекс опорного элемента.\n"
+                        f"Ожидалось: {want_indx}\n"
+                        f"Получено:  {indx}"
+                    )
+                )
+
+if __name__ == "__main__":
+    unittest.main()
