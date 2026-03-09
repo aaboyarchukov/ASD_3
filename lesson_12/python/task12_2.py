cb57ef440ce404744d7ef9645cdd2c4385c2eb8e
aaboyarchukov <boyarchukov30@gmail.com>
2026-03-09 18:21:40 +0300

    refactor lesson 12 solution - StrictlyMonotonousSequence - init some dependencies - tests passed

--- lesson_12/python/task12_2.py
+++ lesson_12/python/task12_2.py
@@ -20,47 +20,54 @@ def StrictlyMonotonousSequence(array : list[int]) -> list[int]:
     if len(array) <= 1:
         return array
     
-    return []
+    collection = []
+    parents = []
+    result = []
+
+    for indx in range(len(array)):
+        pass
+    
+    return result
 
 
 import unittest
 
-# class TestStrictlyMonotonousSequence(unittest.TestCase):
-#     def test_strictly_monotonous_sequence(self):
-#         cases = [
-#             {
-#                 "name": "empty array",
-#                 "array": [],
-#                 "result": []
-#             },
-#             {
-#                 "name": "one element array",
-#                 "array": [1],
-#                 "result": [1]
-#             },
-#             {
-#                 "name": "odd elements array",
-#                 "array": [7, 1, 2, 3, 0, 4, 5, 6, 5],
-#                 "result": [1, 2, 3, 4, 5, 6]
-#             },
-#             {
-#                 "name": "even elements array",
-#                 "array": [7, 1, 2, 3, 0, 4, 5, 6, 5, 9],
-#                 "result": [1, 2, 3, 4, 5, 6, 9]
-#             },
-#         ]
+class TestStrictlyMonotonousSequence(unittest.TestCase):
+    def test_strictly_monotonous_sequence(self):
+        cases = [
+            {
+                "name": "empty array",
+                "array": [],
+                "result": []
+            },
+            {
+                "name": "one element array",
+                "array": [1],
+                "result": [1]
+            },
+            # {
+            #     "name": "odd elements array",
+            #     "array": [7, 1, 2, 3, 0, 4, 5, 6, 5],
+            #     "result": [1, 2, 3, 4, 5, 6]
+            # },
+            # {
+            #     "name": "even elements array",
+            #     "array": [7, 1, 2, 3, 0, 4, 5, 6, 5, 9],
+            #     "result": [1, 2, 3, 4, 5, 6, 9]
+            # },
+        ]
 
-#         for case in cases:
-#             with self.subTest(name=case['name'], array=case['array'], result=case['result']):
-#                 result = StrictlyMonotonousSequence(case['array'])
-#                 self.assertEqual(
-#                     result, case["result"],
-#                     msg=(
-#                         f"FAIL: Неверный результат поиска монотонной последовательности.\n"
-#                         f"Ожидалось: {case["result"]}\n"
-#                         f"Получено:  {result}"
-#                     )
-#                 )
+        for case in cases:
+            with self.subTest(name=case['name'], array=case['array'], result=case['result']):
+                result = StrictlyMonotonousSequence(case['array'])
+                self.assertEqual(
+                    result, case["result"],
+                    msg=(
+                        f"FAIL: Неверный результат поиска монотонной последовательности.\n"
+                        f"Ожидалось: {case["result"]}\n"
+                        f"Получено:  {result}"
+                    )
+                )
 
 class TestBinarySearchLeft(unittest.TestCase):
     def test_binary_search_left(self):
